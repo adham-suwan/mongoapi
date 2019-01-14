14632d4e22e4c608f8994a80e95a4ee6a6540e97
adham-suwan <a.suwan@msn.com>
2019-01-14 10:57:25 +0200

    first commit

---
+++ MongoApi/Services/BookService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("BookstoreDb"));
+            var database = client.GetDatabase("BooksDb");
+            _books = database.GetCollection<Book>("Test");
+        }
+
+        public List<Book> Get()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book Get(string id)
+        {
+            var docId = new ObjectId(id);
+
+            return _books.Find<Book>(book => book.Id == docId).FirstOrDefault();
+        }
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn)
+        {
+            var docId = new ObjectId(id);
+
+            _books.ReplaceOne(book => book.Id == docId, bookIn);
+        }
+
+        public void Remove(Book bookIn)
+        {
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+        }
+
+        public void Remove(ObjectId id)
+        {
+            _books.DeleteOne(book => book.Id == id);
+        }
+    }
+}
